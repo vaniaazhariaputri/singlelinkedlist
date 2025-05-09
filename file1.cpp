a353c2cc113b59ea3706d88d4e52d7ef329dc37d
vaniaazhariaputri <vaniaazharia@gmail.com>
2025-05-09 09:21:25 +0700

    Memasukkan no mahasiswa yang dicari dan data ditemukan, membuat case 5 dan pilihan salah

--- file1.cpp
+++ file1.cpp
@@ -169,4 +169,31 @@ int main()
                 cout << "\nList Kosong\n";
                 break;
             }
+            Node *previous, *current;
+            cout << endl
+                << "Masukkan no mahasiswa yang dicari : ";
+                cin >> nim;
+                if (mhs.Search(nim, &previous, &current) == false)
+                    cout << endl
+                    << "Data tidak ditemukan" << endl;
+                else
+                {
+                    cout << endl
+                        << "Data ditemukan" << endl;
+                    cout << "\nNo Mahasiswa: " << current->noMhs << endl;
+                    cout << "\n";
+                }
+            }
+            break;
+            case '5':
+            {
+            }
+            break;
+            default:
+            {
+                cout << "Pilihansalah !." << endl;
+            }
+            break;
         }
+    } while (ch != '5');
+}
