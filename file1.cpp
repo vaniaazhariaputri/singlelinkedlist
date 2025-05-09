b22e0e5f0ad329759decc0c74c38fa288c5b045c
vaniaazhariaputri <vaniaazharia@gmail.com>
2025-05-09 08:14:33 +0700

    Membuat perulangan nim

--- file1.cpp
+++ file1.cpp
@@ -39,5 +39,24 @@ public:
             START = nodeBaru;
             return;
         }
+
+        Node *previous = START;
+        Node *current = START;
+
+        while ((current != NULL) && (nim >= current->noMhs))
+        {
+            if (nim == current->noMhs)
+            {
+                cout << "\nDuplikasi noMhs tidak diijinkan\n";
+                return;
+            }
+            previous = current;
+            current = current->next;
+        }
+
+        nodeBaru->next = current;
+        previous->next = nodeBaru;
+    }
 }
-};
+
+
