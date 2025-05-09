d4da0f206aa29d16d44680ea8d3316fd18c80c29
vaniaazhariaputri <vaniaazharia@gmail.com>
2025-05-09 08:48:52 +0700

    Memanggil LinkedList nim dan menambahkan menu

--- file1.cpp
+++ file1.cpp
@@ -9,12 +9,12 @@ public:
     Node *next;
 };
 
-class SingleLinkedList
+class LinkedList
 {
     Node *START;
 
 public:
-    SingleLinkedList()
+    LinkedList()
     {
         START =NULL;
     }
@@ -111,6 +111,26 @@ public:
         }
     }
 };
+
+int main()
+{
+    LinkedList mhs;
+    int nim;
+    char ch;
+
+    do
+    {
+        cout << "Menu" << endl;
+        cout << "1. Menambahkan data kedalam list" << endl;
+        cout << "2. Menghapus data dari dalam list" << endl;
+        cout << "3. Menampilkan semua data didalam list" << endl;
+        cout << "4. Mencari data dalam list" << endl;
+        cout << "5. Keluar" << endl;
+        cout << endl
+            << "Masukkan pilihan (1-5) ";
+        cin >> ch;
+        switch (ch)
+    }
     }
 
 
