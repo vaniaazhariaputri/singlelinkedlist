5fa8778032cc2d601c2575925971d63d02e252ed
vaniaazhariaputri <vaniaazharia@gmail.com>
2025-05-09 08:28:38 +0700

    menambahkan current dalam delNode

--- file1.cpp
+++ file1.cpp
@@ -73,6 +73,24 @@ public:
             *previous = *current;
             *current = (*current)->next;
         }
+
+        return (*current != NULL);
+    }
+
+    bool delNode(int nim)
+    {
+        Node *current, *previous;
+        if (!Search(nim, &previous, &current))
+        return false;
+
+        if (current == START)
+            START = START->next;
+        else
+            previous->next = current->next;
+
+            delete current;
+            return true;
+    }
     }
 
 
