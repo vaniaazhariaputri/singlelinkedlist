c0a343617ee2e505effd352164f7e943bb5661ba
vaniaazhariaputri <vaniaazharia@gmail.com>
2025-05-09 08:34:23 +0700

    Menambahkan data didalam list

--- file1.cpp
+++ file1.cpp
@@ -91,6 +91,26 @@ public:
             delete current;
             return true;
     }
+
+    void traverse()
+    {
+        if (listEmpty())
+        {
+            cout << "\nList Kosong\n";
+        }
+        else
+        {
+            cout << "\nData didalam list adalah:\n";
+            Node *currentNode = START;
+            while (currentNode != NULL)
+            {
+                cout << currentNode->noMhs << endl;
+                currentNode = currentNode->next;
+            }
+            cout << endl;
+        }
+    }
+};
     }
 
 
